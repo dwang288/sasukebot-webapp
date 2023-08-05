5f4078a39ab899e24903e9a723179c194178cdf7
Duan <duan.wang93@gmail.com>
2023-08-05 13:03:17 -0400

    add fileserver and static assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,12 +6,20 @@ import (
 )
 
 func main() {
+	// Create a file server for serving static files out of a directory
+	// Path given is relative to the project directory root
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	// Use the http.NewServeMux() function to initialize a new servemux
 	// Register the home function as the handler for the "/" URL pattern
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	// Register FileServer as the handler for URL paths that start with /static/
+	// Strip /static prefix from URL path before processing request
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Use the http.ListenAndServe() function to start a new web server
 	// Pass in the port and the servemux
