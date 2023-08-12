5af7d1ff4a0d644e987f128c7517095023a55e7d
Duan <duan.wang93@gmail.com>
2023-08-12 03:24:22 -0400

    render snippet form submission template, parse form data on post

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -64,18 +64,32 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "view.tmpl.html", data)
 }
 
-// Placeholder function
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	// Removing check for post
 
-	// Example dummy data
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+	// Adds data in POST request bodies to the r.PostForm map
+	// Function also works for PUT and PATCH
+	err := r.ParseForm()
+	if err != nil {
+		// Client is notified of any bad requests
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Grab body data
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
 	// Pass data to Insert method and receive ID of the inserted method back
 	id, err := app.snippets.Insert(title, content, expires)
